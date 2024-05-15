bca0438cbb1be68c237c87fbf67722b9aa99ca96
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-05-15 12:58:09 +0100

    Setup connection to local MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Experience = require('./models/experience');
+
+mongoose.connect('mongodb://localhost:27017/sekai-experience');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +20,14 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/makeexperience', async (req, res) => {
+    const exp = new Experience({
+        title: 'Your First Experience',
+        location: 'Somewhere in the world',
+        description: 'Something life changing.' });
+        await exp.save();
+        res.send(exp);
+})
 
 app.listen(3000, () => {
     console.log('Serving data on port 3000')
