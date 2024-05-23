07d8e731d061d896cf42e0d9daa5786c6b993bd5
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-05-23 11:41:38 +0100

    Create and link review model to experience model

--- models/experience.js
+++ models/experience.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
+const review = require('./review');
 const Schema = mongoose.Schema;
 
 const ExperienceSchema = new Schema({
     title: String,
     image: String,
     location: String,
-    description: String
+    description: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Experience', ExperienceSchema);
