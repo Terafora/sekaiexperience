b7251deb9d7c5134737d9e6c3421448be2175e53
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-05-24 14:22:49 +0100

    Setup session config/cookies

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+
 
 const experiences = require('./routes/experiences');
 const reviews = require('./routes/reviews');
@@ -24,6 +26,19 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret : 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 app.use("/experiences", experiences);
 app.use("/experiences/:id/reviews", reviews);
