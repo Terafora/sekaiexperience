20667ff5be83f1ccb6e659c358ce48fc4abf3e72
Charlotte Stone <charlie.stone649@gmail.com>
2024-05-29 15:45:45 +0100

    Improve redirection after login

--- routes/users.js
+++ routes/users.js
@@ -3,42 +3,48 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const { isLoggedIn, storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchAsync (async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
-        const user = new User({email, username});
+        const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to Sekai Experience!');
-        res.redirect('/experiences');
-    } catch(e) {
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Sekai Experience!');
+            res.redirect('/experiences');
+        });
+    } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
     }
 }));
 
-router.get('/login', (req, res) => {
+router.get('/login', storeReturnTo, (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),(req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/experiences');
+    const redirectUrl = res.locals.returnTo || '/experiences';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res, next) => {
-    req.logout(function(err){
-        if(err) {
+    req.logout(function (err) {
+        if (err) {
             req.flash('error', 'Cannot log out');
             return next(err);
         }
         req.flash('success', 'Goodbye!');
         res.redirect('/experiences');
-    })    
+    });
 });
 
 module.exports = router;
