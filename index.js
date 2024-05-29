c9d0104f1d6afd961e80479a92519e1b0ce6fa16
Charlotte Stone <charlie.stone649@gmail.com>
2024-05-29 12:54:13 +0100

    Setup basic authorisation with Passport and test hard coded info

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const experiences = require('./routes/experiences');
 const reviews = require('./routes/reviews');
@@ -45,10 +48,28 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+});
+
+app.get('/fakeUser', async (req, res) => {
+    try {
+        const user = new User({ email: 'test@email.com', username: 'robot' });
+        const newUser = await User.register(user, 'monkey');
+        res.send(newUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred');
+    }
 });
 
 app.use("/experiences", experiences);
