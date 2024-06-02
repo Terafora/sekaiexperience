2d90670944af96da59c4d3027dfaf91c167eeb5c
Charlotte Stone <charlie.stone649@gmail.com>
2024-06-02 12:47:27 +0100

    Refactor user routing logic into controller file

---
+++ controllers/users.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+
+module.exports.renderRegister = (req, res) => {
+    res.render('users/register');
+};
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Sekai Experience!');
+            res.redirect('/experiences');
+        });
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+};
+
+module.exports.renderLogin = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.login = (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = res.locals.returnTo || '/experiences';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            req.flash('error', 'Cannot log out');
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/experiences');
+    });
+};
